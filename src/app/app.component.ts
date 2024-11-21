f7489ea3ffd83d1a342c34d7f6ede347f8d9be4e
Filonenko Dmytro <filonenkodmytrodm@gmail.com>
2024-11-21 21:05:47 +0200

    added general styles

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,6 +5,7 @@ import { HeaderComponent } from './header/header.component';
   selector: 'app-root',
   standalone: true,
   templateUrl: './app.component.html',
+  styleUrl: './app.component.css',
   imports: [HeaderComponent],
 })
 export class AppComponent {}
